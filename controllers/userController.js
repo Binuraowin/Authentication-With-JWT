e6b2a7bb1aa244ba6abe9ae18238b7ae10ef91be
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2021-10-15 14:58:44 +0530

    validation

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,12 @@
 const User = require('../model/UserModel');
-const joi = require('joi');
 const mongoose = require("mongoose");
+const {registerValidation} = require('../validator')
 
-const schema = joi.object({
-    name: joi.string().min(6).required(),
-    email: joi.string().min(6).required().email(),
-    password: joi.string().min(6).required(),
-})
+
 
 exports.register = async (req, res, next) => {
     
-    const {error} = schema.validate(req.body)
+    const {error} = registerValidation(req.body)
     if(error) return res.status(400).send(error.details[0].message)
           const user = new User({
             _id: new mongoose.Types.ObjectId(),
